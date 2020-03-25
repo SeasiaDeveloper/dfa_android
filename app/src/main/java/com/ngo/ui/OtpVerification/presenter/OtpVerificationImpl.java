2723c2f8f6d4a25ec74fc685e67066ec6e1f63e3
ajay <ajay.seasia@outlook.com>
2020-03-25 21:42:56 +0530

    -commit by sudisha (25-03-20)

--- app/src/main/java/com/ngo/ui/OtpVerification/presenter/OtpVerificationImpl.java
+++
@@ -1,4 +0,0 @@
-package com.ngo.ui.OtpVerification.presenter;
-
-public class OtpVerificationImpl {
-}
---
+++ app/src/main/java/com/ngo/ui/OtpVerification/view/OtpVerificationView.kt
@@ -0,0 +1,7 @@
+package com.ngo.ui.OtpVerification.view
+
+import com.ngo.base.view.BaseView
+
+interface OtpVerificationView : BaseView {
+
+}
