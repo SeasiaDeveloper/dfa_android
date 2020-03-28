08ff6ddb26ae710c1c4ea035462271510b91f4f9
developer.seasia1 <developer.seasia1@gmail.com>
2020-03-28 14:48:03 +0530

    28_03_2020: Commit by Harman Delete api implemented(are you sure you want to del dialog pending)

--- app/src/main/java/com/ngo/listeners/OnCaseItemClickListener.kt
+++ app/src/main/java/com/ngo/listeners/OnCaseItemClickListener.kt
@@ -4,6 +4,10 @@ import com.ngo.pojo.response.GetCasesResponse
 
 interface OnCaseItemClickListener {
     fun onItemClick(complaintsData: GetCasesResponse.DataBean, type:String)
+
+    //to delete the complaint/post
     fun onDeleteItem(complaintsData: GetCasesResponse.DataBean)
+
+    //to change the like status
     fun changeLikeStatus(complaintsData: GetCasesResponse.DataBean)
 }
